78cbbc18e2e528eb86f6715b6b31fcfda5f102e8
mamuncseru <mamun.cseian@gmail.com>
2021-09-24 11:29:26 +0600

    Dynamic url learning

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,7 +4,9 @@ from . import views;
 
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february),
-    path("march", views.march),
+    # path("january", views.january),
+    # path("february", views.february),
+    # path("march", views.march),
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,48 @@
+import challenges
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("This is January!")
+# def january(request):
+#     return HttpResponse("This is January!")
 
-def february(request):
-    return HttpResponse("This is february...")
+# def february(request):forword_month
+#     return HttpResponse("This is february...")
 
-def march(request):
-    return HttpResponse("Learn Django for at least 20 minutes every day.")
+# def march(request):
+#     return HttpResponse("Learn Django for at least 20 minutes every day.")
 
+monthly_challenges = {
+    "january": 'Eat no meat for the entire month',
+    "february": 'Walk for at least 20 minutes',
+    "march": 'Learn Django for at least 20 minutes every day!',
+    "april": 'Learn python for at least 20 minutes every day!',
+    "May": 'Learn Javascript for at least 20 minutes every day!',
+    "june": 'Learn php for at least 20 minutes every day!',
+    "july": 'Learn c for at least 20 minutes every day!',
+    "august": 'Learn c++ for at least 20 minutes every day!',
+    "september": 'Learn java for at least 20 minutes every day!',
+    "october": 'Learn tensorflow for at least 20 minutes every day!',
+    "november": 'Learn pytorch for at least 20 minutes every day!',
+    "december": 'Learn Kotlin for at least 20 minutes every day!',
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month-1]
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
 
